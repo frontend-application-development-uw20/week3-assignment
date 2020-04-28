7405f74970e56136fab2e6b010a3bcf4c867b266
chanjessica <jchan_sea@yahoo.com>
2020-04-27 23:23:53 -0700

    add total

--- ecommerce-ui/src/cart.js
+++ ecommerce-ui/src/cart.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import cart from './cart-icon.svg'
 
 class ShoppingCart extends Component {
+
     static propTypes = {
         checkout: PropTypes.arrayOf(
             PropTypes.shape({
@@ -20,17 +21,19 @@ class ShoppingCart extends Component {
         // alert('checkout.length ' + checkout.length + ' ' + JSON.stringify(checkout));
         return (
             <aside>
-                <img src={cart} className="shoppingCart" alt="logo" />
+                <img src={cart} className="shoppingCart" alt="Shopping Cart" />
                 <ul>
                     {checkout.length === 0 ? (<p>No Rental Selected</p>) : (
 
                         checkout.map((cart) =>
                             <li key={cart.id}>   {cart.rental}   ${cart.price} /night
-                        <button onClick={removeRental(cart.id, cart.rental
+                                <button onClick={removeRental(cart.id, cart.rental
                             )} type="button">Remove</button>
                             </li>)
+
                     )
                     }
+                    <span className="total">Total = ${checkout.reduce((total, rental) => parseFloat(total) + parseFloat(rental.price), 0)}</span>
                 </ul>
             </aside>
         )
--- ecommerce-ui/src/shoppingCart.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function shoppingCart() {
-    const cart = [{
-        rental: 'hotel',
-        price: 200
-    }];
-    return cart
-}
