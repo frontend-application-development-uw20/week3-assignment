ab1f66292f9882855738e488e983af26e66369bb
supriyapuri <supriyapuri11@gmail.com>
2020-05-06 22:08:38 -0700

    moved components files from src to sub-folder

---
+++ ecommerce-ui/src/Components/Cart.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+
+
+
+
+
+export default class Cart extends React.Component {
+
+
+
+    render() {
+        const {cartList} = this.props;
+        
+        
+        
+        return (
+            <div>
+                <h2>Shopping Cart</h2>
+                {cartList.length===0 ? "Cart is empty" : <div>  {cartList.length} rental selected</div>}
+                {cartList.length> 0&&
+                    <div>
+                        <ul>
+                            {cartList.map((item, key) =>
+                            <li key={key}>
+                                <b>{item.title} - ${item.cost}</b> 
+                                <button className= "RemoveCart" onClick={(e) =>this.props.handleRemoveFromCart(e,item)}>
+                                    
+                                    x</button>
+                            </li>
+                            
+                            )}
+                        </ul>
+                        Total :${cartList.reduce((a,c)=> parseFloat(a) + parseFloat(c.cost), 0)} 
+
+                    </div>
+                
+                }
+              
+            </div>
+            
+          
+                    
+                    
+  
+               
+        ) ;
+    }
+
+}
+
+
+
